b1b32fdfe9afd04010cd0e56050f38063f7bc871
yushan <yushanwebdev@gmail.com>
2022-08-08 08:45:58 +0530

    feat: Lottie - Install the `lottie-react-native` & `lottie-ios` packages - Add Lottie Animation to the <Cart />

--- src/view/screens/cart/Component.tsx
+++ src/view/screens/cart/Component.tsx
@@ -4,11 +4,21 @@ import {Props} from '.';
 import store from '../../../../shared/redux/store';
 import {BUTTON_DEFAULT} from '../../elements/buttons';
 import {GLOBAL} from '../../styles/global';
+import Lottie from 'lottie-react-native';
 
 const Cart: React.FC<Props> = props => {
   return (
     <SafeAreaView style={GLOBAL.LAYOUT.SafeArea}>
       <View style={GLOBAL.LAYOUT.pageContainer}>
+        <Lottie
+          source={require('../../assets/animations/car.json')}
+          autoPlay
+          loop
+          style={{
+            width: 200,
+            height: 200,
+          }}
+        />
         <BUTTON_DEFAULT
           title={`Add to Cart ${store.getState().cart.items.length}`}
           onClick={() =>
